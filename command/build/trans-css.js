aaf211ec400c2225d1bd5297fa9073f30a060a64
super <0@00010101.com>
2015-11-02 14:08:00 +0800

    fix bug

--- command/build/trans-css.js
+++ command/build/trans-css.js
@@ -3,7 +3,7 @@ var transJs = require("./trans-js");
 var template = require("fs").readFileSync(__dirname + "/css-template.tpl", "utf8");
 
 module.exports = function(mod, code, callback){
-	var css = template.replace("{{style}}", new cleanCSS().minify(code).replace(/\\/g, "\\\\").replace(/'/g, "\\\'"));
+	var css = template.replace("{{style}}", new cleanCSS().minify(code).replace(/[\r\n\t]/g, " ").replace(/\\/g, "\\\\").replace(/'/g, "\\\'"));
 
 	transJs(mod, css, callback);
 };
--- command/build/trans-txt.js
+++ command/build/trans-txt.js
@@ -2,7 +2,7 @@ var transJs = require("./trans-js");
 var template = require("fs").readFileSync(__dirname + "/txt-template.tpl", "utf8");
 
 module.exports = function(mod, code, callback){
-	var css = template.replace("{{content}}", code.replace(/\n+/g, "").replace(/\\/g, "\\\\").replace(/'/g, "\\\'"));
+	var css = template.replace("{{content}}", code.replace(/[\r\n\t]/g, " ").replace(/\\/g, "\\\\").replace(/'/g, "\\\'"));
 
 	transJs(mod, css, callback);
 };
